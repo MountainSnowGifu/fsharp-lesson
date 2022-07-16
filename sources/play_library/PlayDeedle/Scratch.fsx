30e14a14b20f1b4ce9bde3f352a6f9d2e806fdcf
akira <akira@akiranoMacBook-Pro.local>
2022-07-16 22:26:23 +0900

    課題4

--- sources/play_library/PlayDeedle/Scratch.fsx
+++ sources/play_library/PlayDeedle/Scratch.fsx
@@ -1,8 +1,6 @@
 #r "nuget:Deedle"
-
 open Deedle
 open System
-
 
 #load "Deedle.fsx"
 Frame.ReadCsv "sources/data/シラバス.csv"
@@ -15,7 +13,7 @@ df.RowsDense
 let row = df.Rows.GetAt(0)
 
 row.Get("専門")
-row.GetAs("専門")
+//row.GetAs("専門")
 row.GetAs<string>("専門")
 row.GetAs<int>("場所")
 row.GetAs<string>("場所")
@@ -28,10 +26,10 @@ row.GetAs<string>("場所")
 df.RowsDense |> Series.filterValues(fun row -> row.GetAs<string>("専門").Contains("数学"))
 
 //課題3: 専門が数学の行だけを持ったFrameを作ろう
-let df2 = Frame.ReadCsv("sources/data/シラバス.csv")
-        |> Frame.filterRowValues(fun row -> row.GetAs<string>("専門").Contains("数学"))
+df |> Frame.filterRowValues(fun row -> row.GetAs<string>("専門").Contains("数学"))
 
 //課題4: 場所と学年だけのFrameを作ろう
-let df3 = Frame.ReadCsv("sources/data/シラバス.csv")
-let df4 = df3.Columns.[ ["場所"; "学年"] ]
+df.Columns.[ ["場所"; "学年"] ]
+df |> Frame.filterCols(fun col _ -> col = "専門")
+
 
