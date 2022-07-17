43af61b7317872a3b797b424a60425410920daa1
akira <akira@akiranoMacBook-Pro.local>
2022-07-17 12:17:06 +0900

    課題7

--- sources/play_library/PlayArgu/Program.fs
+++ sources/play_library/PlayArgu/Program.fs
@@ -1,9 +1,23 @@
 ﻿open System
+open Argu
 
-//課題6: オプションhelloを処理[<EntryPoint>]
+[<CliPrefix(CliPrefix.Dash)>]
+type Arguments =
+    | Hello
+
+    interface IArgParserTemplate with
+      member s.Usage =
+        match s with
+        | Hello -> "print Hello World"
+
+
+//課題7: -hello をArgu化しよう
 [<EntryPoint>]
 let main args =
-    if  args.Length = 1 && args[0] = "-hello" then
+    printfn "Arguments passed to function : %A" args
+    let parser = ArgumentParser.Create<Arguments>(programName="PlayArgu")
+    let res = parser.Parse args
+    if res.Contains Hello then
         printfn "hello world"
     else
         printfn "I don't know"
