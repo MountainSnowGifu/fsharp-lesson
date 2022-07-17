766bbfbfe062a20cd42d3c8dc1c14719b2110103
akira <akira@akiranoMacBook-Pro.local>
2022-07-17 11:45:01 +0900

    arguを追加

--- sources/play_library/PlayArgu/Scratch.fsx
+++ sources/play_library/PlayArgu/Scratch.fsx
@@ -2,3 +2,30 @@
 //git add sources/play_library/PlayArgu/Scratch.fsx
 //git commit -m '課題6-2'
 //git push -u origin play_library/2_helloargu   
+#r "nuget: Argu"
+open Argu
+
+type Arguments =
+    | Hoge
+    | Foo of path:string
+
+    interface IArgParserTemplate with
+      member s.Usage =
+        match s with
+        | Hoge -> "print Hello World"
+        | Foo _ -> "print Bar with arg"
+
+let parser = ArgumentParser.Create<Arguments>(programName="hogehoge")
+
+parser.PrintUsage()
+
+parser.Parse[| "--hoge" |]
+parser.Parse[| "--foo" |] //error
+parser.Parse[| "--foo"; "fugafuga" |]
+
+let res = parser.Parse[| "--foo"; "ikaika" |]
+res.Contains Hoge
+res.Contains Foo
+res.GetResult(Foo)
+
+let res2 = parser.Parse[| "--hoge"; "--foo"; "ikaika" |]
