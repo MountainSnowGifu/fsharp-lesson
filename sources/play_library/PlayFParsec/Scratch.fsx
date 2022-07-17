f2b030bf45052907b49793c2a66e241e876c63bc
akira <akira@akiranoMacBook-Pro.local>
2022-07-17 22:29:33 +0900

    課題9

--- sources/play_library/PlayFParsec/Scratch.fsx
+++ sources/play_library/PlayFParsec/Scratch.fsx
@@ -7,7 +7,7 @@ open FParsec
 run pfloat "1.25"
 
 let ws = spaces
-let float_ws = pfloat .>> ws
+let float_ws : Parser<float,unit> = pfloat .>> ws
 run float_ws "1.25"
 
 let test p str =
@@ -24,3 +24,38 @@ let (floatBetweenBrackets: Parser<float,unit>) = str "[" >>. pfloat .>> str "]"
 test floatBetweenBrackets "[1.0]"
 test floatBetweenBrackets "[]"
 test floatBetweenBrackets "[1.0"
+
+//4.4 Abstracting parsers
+let betweenStrings s1 s2 p = str s1 >>. p .>> str s2
+let floatBetweenBrackets2 : Parser<float,unit> = pfloat |> betweenStrings "[" "]"
+let floatBetweenDoubleBrackets2: Parser<float,unit> = pfloat |> betweenStrings "[[" "]]"
+test floatBetweenBrackets2 "[1.0]"
+test floatBetweenDoubleBrackets2 "[[1.0]]"
+
+let between pBegin pEnd p  = pBegin >>. p .>> pEnd
+let betweenStrings2 s1 s2 p = p |> between (str s1) (str s2)
+let floatBetween3Brackets: Parser<float,unit> = pfloat |> betweenStrings "[[[" "]]]"
+test floatBetween3Brackets "[[[1.0]]]"
+
+//4.5 Parsing a list of floats
+test (many floatBetweenBrackets) ""
+test (many floatBetweenBrackets) "[1.0]"
+test (many floatBetweenBrackets) "[2][3][4]"
+test (many floatBetweenBrackets) "[1][2.0E]"
+test (many1 floatBetweenBrackets) "(1)"
+test (many1 (floatBetweenBrackets <?> "float between brackets")) "(1)"
+
+//4.6 Handling whitespace
+test floatBetweenBrackets "[1.0, 2.0]"
+//let ws = spaces
+let str_ws s = pstring s .>> ws
+//let float_ws = pfloat .>> ws
+let numberList = str_ws "[" >>. sepBy float_ws (str_ws ",") .>> str_ws "]"
+
+test numberList @"[ 1 ,
+                          2 ] "
+test numberList @"[ 1,
+                         2; 3]"
+let numberListFile = ws >>. numberList .>> eof
+test numberListFile " [1, 2, 3] [4]"
+test numberListFile " [1, 2, 3,4]"
