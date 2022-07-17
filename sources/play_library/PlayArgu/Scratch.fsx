a77a895e297420d67e8a778aab60c931dba01464
akira <akira@akiranoMacBook-Pro.local>
2022-07-17 11:14:15 +0900

    .gitignore test

--- sources/play_library/PlayArgu/Scratch.fsx
+++
@@ -1,4 +0,0 @@
-//git add /Users/akira/Desktop/F#/F#Lesson/fsharp-lesson/sources/play_library/PlayArgu/Program.fs
-//git add sources/play_library/PlayArgu/Scratch.fsx
-//git commit -m '課題6-2'
-//git push -u origin play_library/2_helloargu   
