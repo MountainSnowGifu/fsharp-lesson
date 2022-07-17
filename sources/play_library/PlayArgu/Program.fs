cf038b7b72efaeabda929a4ffb337fb3a1d3f2cc
akira <akira@akiranoMacBook-Pro.local>
2022-07-17 14:00:23 +0900

    課題8-2

--- sources/play_library/PlayArgu/Program.fs
+++ sources/play_library/PlayArgu/Program.fs
@@ -4,16 +4,21 @@ open Argu
 
 [<CliPrefix(CliPrefix.DoubleDash)>]
 type Arguments =
-    | Project of List<string>
+    | Project of string list
+    | Filter of string list
 
     interface IArgParserTemplate with
       member s.Usage =
         match s with
         | Project _ -> "Project"
+        | Filter _ -> "Filter"
 
-let project (list:list<string>) (frame :Frame<int,string>) = frame.Columns.[list]
+let project (columnList:string list) (frame :Frame<int,string>) = frame.Columns.[columnList]
+let f (list: string list) = fun (row: ObjectSeries<string>) -> row.GetAs<string>(list[0]).Contains(list[1])
+let filter f frame :Frame<int,string>  = frame |> Frame.filterRowValues f
 
 //課題8: PlayDeedleのprojectとfilterをArguでコマンドライン化
+//課題8-2: filter
 [<EntryPoint>]
 let main args =
     let df = Frame.ReadCsv "sources/data/シラバス.csv"
@@ -21,6 +26,12 @@ let main args =
     let res = parser.Parse args
 
     if res.Contains Project then
-        let list = df|> project (res.GetResult(Project))
+        let selected_df = df |> project (res.GetResult(Project))
+        selected_df.Print()
+
+    if res.Contains Filter then
+        let prediction = f (res.GetResult(Filter))
+        let list = df |> filter prediction
         list.Print()
+
     0
