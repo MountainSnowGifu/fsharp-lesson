6bace1ad42a5c5fb8b3562983158273b13ef44f6
akira <akira@akiranoMacBook-Pro.local>
2022-07-17 14:35:51 +0900

    課題9

--- sources/play_library/PlayFParsec/Scratch.fsx
+++ sources/play_library/PlayFParsec/Scratch.fsx
@@ -1,3 +1,24 @@
 //git add /Users/akira/Desktop/F#/F#Lesson/fsharp-lesson/sources/play_library/PlayFParsec
 //git commit -m '課題6-2'
 //git push -u origin play_library/3_playfparsec 
+
+#r "nuget: FParsec"
+open FParsec
+run pfloat "1.25"
+
+let ws = spaces
+let float_ws = pfloat .>> ws
+run float_ws "1.25"
+
+let test p str =
+    match run p str with
+    | Success(result, _, _)   -> printfn "Success: %A" result
+    | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
+
+test pfloat "1.25"
+test pfloat "1.25E 3"
+
+let str s = pstring s
+let (floatBetweenBrackets: Parser<float,unit>) = str "[" >>. pfloat .>> str "]"
+test floatBetweenBrackets "[1.0]"
+test floatBetweenBrackets "[]"
