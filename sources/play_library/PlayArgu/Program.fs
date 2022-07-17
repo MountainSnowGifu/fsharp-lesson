6604f12e28697f0fca5231e5f4bfac48912beb21
akira <akira@akiranoMacBook-Pro.local>
2022-07-17 10:38:49 +0900

    課題6: オプションhelloを処理2

--- sources/play_library/PlayArgu/Program.fs
+++ sources/play_library/PlayArgu/Program.fs
@@ -1,13 +1,21 @@
 ﻿open System
 
-let showCommandLineArgs() =
+//課題6: オプションhelloを処理
+// let showCommandLineArgs() =
 
-    let args = System.Environment.GetCommandLineArgs()
-    if  args.Length = 2 && args[1] = "-hello" then
+//     let args = System.Environment.GetCommandLineArgs()
+//     if  args.Length = 2 && args[1] = "-hello" then
+//         printfn "hello world"
+//     else
+//         printfn "I don't know"
+//showCommandLineArgs()
+//exit 100
+
+//課題6: オプションhelloを処理[<EntryPoint>]
+[<EntryPoint>]
+let main args =
+    if  args.Length = 1 && args[0] = "-hello" then
         printfn "hello world"
     else
         printfn "I don't know"
-
-
-showCommandLineArgs()
-exit 100
+    0
